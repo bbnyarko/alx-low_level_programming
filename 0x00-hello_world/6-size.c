c01e147787d2b0ea2f6e6532e845d4f574b15371
Benjamin Nyarko <bbnyarko11@gmail.com>
2023-02-16 20:35:44 -0800

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,17 +1,23 @@
-#!/bin/bash
 #include <stdio.h>
 
-/
+/**
 *main -Entry point
+*
 *Return: Always 0 (Success)
+*
 */
-
 int main(void)
 {
-        printf("Size of a char: %zu byte(s)\n", sizeof(char));
+        int charSize = sizeof(char);
+	int intSize = sizeof(int);
+	int longsize = sizeof(long int);
+	int longlongSize = sizeof(long long int)
+	int floatSize = sizeof(float);
+
+	printf("Size of a char: %zu byte(s)\n", sizeof(char));
         printf("Size of an int: %zu byte(s)\n", sizeof(int));
         printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
         printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
         printf("Size of a float: %zu byte(s)\n", sizeof(float));
-                return (0);
+        return (0);
 }
