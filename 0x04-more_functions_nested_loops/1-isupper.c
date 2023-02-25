232004c61229ee36d94f501e498c34b6b0e0acc6
Benjamin Nyarko <bbnyarko11@gmail.com>
2023-02-24 16:23:15 -0800

    uppercase

--- 0x04-more_functions_nested_loops/1-isupper.c
+++ 0x04-more_functions_nested_loops/1-isupper.c
@@ -2,13 +2,13 @@
 
 /**
   * _isupper - Check if a letter is upper
-  * @x: The number to be checked
+  * @c: The number to be checked
   *
   * Return: 1 for upper letter or 0 for any else
   */
-int _isupper(int x)
+int _isupper(int c)
 {
-	if ((x >= 'A') && (x <= 'Z'))
+	if ((c >= 'A') && (c <= 'Z'))
 	{
 		return (1);
 	}
