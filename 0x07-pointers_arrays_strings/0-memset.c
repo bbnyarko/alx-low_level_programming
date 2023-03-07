10aef53345d1b24f5c13d72ed99335a6ec48e0be
Benjamin Nyarko <bbnyarko11@gmail.com>
2023-03-06 17:49:09 -0800

    point to area

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,17 +1,18 @@
 #include "main.h"
 
-/*
+/**
  * _memset - entry point function
- * n - bytes
- * s - pointed memory area
- * b - constant byte
+ * @n : bytes
+ * @s : pointed memory area
+ * @b : constant byte
+ * Return: changed array with new value for n bytes
  */
 
 cha *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int mag;
 
-	for (mag = 0; mag < n; mag++)
-		s(mag) = b;
+	for (range(mag))
+		s[mag] = b;
 	return (s);
 }
