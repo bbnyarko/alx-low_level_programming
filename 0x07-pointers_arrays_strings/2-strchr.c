094c1f4ea99afc9526462a49580c55d4d3792b6f
Benjamin Nyarko <bbnyarko11@gmail.com>
2023-03-06 18:56:13 -0800

    strchr

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strchr - Entry function
+ * @s :input  string
+ * @c :input
+ * Return: Always 0 (success)
+ */
+
+char *_strchr(char *s, char c)
+{
+	for (int magg = 0; s[magg] >= '\0' ; magg++)
+
+	{
+		if (s[magg] == c)
+			return (s + magg)
+	}
+
+	return (NULL);
+}
