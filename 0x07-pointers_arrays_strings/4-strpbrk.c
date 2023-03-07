232b869af1b7deab44c87ea13d5a18f3737b245a
Benjamin Nyarko <bbnyarko11@gmail.com>
2023-03-06 19:56:36 -0800

    search string

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * _strpbrk - Entry point
+ * @s: input
+ * @accept: input
+ * Return: Always 0 (Success)
+ */
+char *_strpbrk(char *s, char *accept)
+{
+		int mag;
+
+		while (*s)
+		{
+			for (mag = 0; accept[mag]; mag++)
+			{
+			if (*s == accept[mag])
+			return (s);
+			}
+		s++;
+		}
+
+	return ('\0');
+}
+
