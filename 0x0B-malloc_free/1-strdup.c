e54188cd06c07f60aeb16a5445588adf2641ccb3
Benjamin Nyarko <bbnyarko11@gmail.com>
2023-03-14 21:01:15 -0700

    strdup

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,45 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+
+
+char *_strdup(char *str);
+{
+
+	char *b;
+
+	int m, r = 0;
+
+
+
+	if (str == NULL)
+
+		return (NULL);
+
+	m = 0;
+
+	while (str[m] != '\0')
+
+		m++;
+
+
+
+	b = malloc(sizeof(char) * (m + 1));
+
+
+
+	if (b == NULL)
+
+		return (NULL);
+
+
+
+	for (r = 0; str[r]; r++)
+
+		b[r] = str[r];
+
+	return (b);
+
+}
+
